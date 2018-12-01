2c4452762dace543b0a56ffa66436b177b94e78e
Bas Verweij <bas@off-sync.com>
2018-12-01 20:16:31 +0100

    Added day 1, puzzle 1.

---
+++ Aoc2018.Day01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aoc2018.Day01
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
